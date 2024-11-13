226e8e70e112b2d4aebc4d15133936b79dac8d86
Giraldy <wawagiraldy@gmail.com>
2024-11-13 21:16:54 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore consectetur in ducimus est iusto culpa qui dolorem soluta perferendis vitae quia alias aliquam
+        praesentium, temporibus maiores fugit, iure nobis mollitia!</p>
+</body>
+
+</html>
